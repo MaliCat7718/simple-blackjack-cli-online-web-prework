13da657900e353b87927ec899251945a8b4103cb
Malary Robinson <robinmal16@gmail.com>
2019-04-25 02:26:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,7 @@ end
 def runner
   welcome
   sleep 0.75
-  total_card = initial_round
+  card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
